7da4916082c6c9e93f614f3d970c62a9eb5ed4e3
Kasia Matuszek <matuszek.kasia@gmail.com>
2017-08-28 15:10:22 +0200

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '8d65c3021be5958ba2d9a2d2d4738fc3';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  };
+}
